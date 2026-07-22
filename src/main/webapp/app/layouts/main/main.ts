c79f586ee59943a067ec0bb1583092bb8f44ce67
ThomasVDB <vandenbergthomas@hotmail.fr>
2026-07-22 22:06:25 +0200

    init componant navbar-cvplt

--- src/main/webapp/app/layouts/main/main.ts
+++ src/main/webapp/app/layouts/main/main.ts
@@ -8,13 +8,14 @@ import { AppPageTitleStrategy } from 'app/app-page-title-strategy';
 import { AccountService } from 'app/core/auth/account.service';
 import Footer from '../footer/footer';
 import PageRibbon from '../profiles/page-ribbon';
+import { NavbarCvplt } from '../navbar/navbar-cvplt/navbar-cvplt';
 
 @Component({
   selector: 'jhi-main',
   changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: './main.html',
   providers: [AppPageTitleStrategy],
-  imports: [RouterOutlet, Footer, PageRibbon],
+  imports: [RouterOutlet, Footer, PageRibbon, NavbarCvplt],
 })
 export default class Main implements OnInit {
   private readonly renderer: Renderer2;
